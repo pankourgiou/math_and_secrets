843f988d38fabc59a530d5e0951592cc9635f9ee
L <60981745+pankourgiou@users.noreply.github.com>
2025-03-03 06:39:46 +0200

    Create math_and_secrets.py

---
+++ math_and_secrets.py
@@ -0,0 +1,119 @@
+import math
+import secrets
+
+def random():
+    return secrets.randbelow(math.floor(2**32))
+
+def random_bytes(n):
+    return secrets.token_bytes(n)
+
+def urandom(n):
+    return secrets.token_bytes(n)
+
+def getrandbits(n):
+    return secrets.randbits(n)
+
+def seed(n):
+    secrets.seed(n)
+import math
+import secrets
+
+def random():
+    return secrets.randbelow(math.floor(2**32))
+
+def random_bytes(n):
+    return secrets.token_bytes(n)
+
+def urandom(n):
+    return secrets.token_bytes(n)
+
+def getrandbits(n):
+    return secrets.randbits(n)
+
+def seed(n):
+    secrets.seed(n)
+import math
+import secrets
+
+def random():
+    return secrets.randbelow(math.floor(2**32))
+
+def random_bytes(n):
+    return secrets.token_bytes(n)
+
+def urandom(n):
+    return secrets.token_bytes(n)
+
+def getrandbits(n):
+    return secrets.randbits(n)
+
+def seed(n):
+    secrets.seed(n)
+import math
+import secrets
+
+def random():
+    return secrets.randbelow(math.floor(2**32))
+
+def random_bytes(n):
+    return secrets.token_bytes(n)
+
+def urandom(n):
+    return secrets.token_bytes(n)
+
+def getrandbits(n):
+    return secrets.randbits(n)
+
+def seed(n):
+    secrets.seed(n)
+import math
+import secrets
+
+def random():
+    return secrets.randbelow(math.floor(2**32))
+
+def random_bytes(n):
+    return secrets.token_bytes(n)
+
+def urandom(n):
+    return secrets.token_bytes(n)
+
+def getrandbits(n):
+    return secrets.randbits(n)
+
+def seed(n):
+    secrets.seed(n)
+import math
+import secrets
+
+def random():
+    return secrets.randbelow(math.floor(2**32))
+
+def random_bytes(n):
+    return secrets.token_bytes(n)
+
+def urandom(n):
+    return secrets.token_bytes(n)
+
+def getrandbits(n):
+    return secrets.randbits(n)
+
+def seed(n):
+    secrets.seed(n)
+import math
+import secrets
+
+def random():
+    return secrets.randbelow(math.floor(2**32))
+
+def random_bytes(n):
+    return secrets.token_bytes(n)
+
+def urandom(n):
+    return secrets.token_bytes(n)
+
+def getrandbits(n):
+    return secrets.randbits(n)
+
+def seed(n):
+    secrets.seed(n)
